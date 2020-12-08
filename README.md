81f91714272f4e1bf89e9f2e1c74df1bfb472b71
uprajapa <59576793+uprajapa@users.noreply.github.com>
2020-12-07 20:52:45 -0500

    Completed till step 3 , 5 and 6

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Group-2-Lab-4-Chapter-9
+# Fundamentals of Web Development, 2nd Edition
+### Chapter 9 [JavaScript 2], Project 3 [Book CRM]
+Write a helper script that uses recursion and which could potentially be used on any
+web page to visually identify the tag name of all elements on a page. Recursion as a
+programming topic was not covered in this chapter, so this project is only suitable
+for a programmer already familiar with the technique.
+
+  
---
+++ js/chapter09-project03.js
@@ -0,0 +1,66 @@
+window.onload = function(){
+document.getElementById("hide").style.display = "none";
+
+document.getElementById("highlight").addEventListener("click", function(){
+
+    var ele = document.querySelectorAll("h1, main, div, p, tr, td, caption, th, a, legend, label, fieldset, button");
+    console.log(ele);
+    highlightRecursive(ele);    
+    
+    document.getElementById("highlight").style.display = "none";
+
+    document.getElementById("hide").style.display = "block";    
+});
+
+document.getElementById("hide").addEventListener("click", function(){
+
+    while(document.geteleByClassName("hoverNode").length > 0){
+    
+        var spanNodes = document.geteleByClassName("hoverNode");
+        
+        for (var i = 0; i < spanNodes.length; i++) {
+        
+            spanNodes[i].remove();
+        
+        }
+        
+    }
+    document.getElementById("highlight").style.display = "block";
+    
+    document.getElementById("hide").style.display = "none";
+    
+    });
+
+}
+
+function highlightRecursive(ele){
+    console.log("nodeType=" + ele.nodeType + " tagName=" + ele.tagName);
+    for (var i = 0; i < ele.length; i++) {
+        // ignore space and text ... we are only interested in element eles
+        if (ele[i].nodeType = 1){
+            var spanEle = document.createElement('SPAN');
+
+            spanEle.className = "hoverNode";
+
+            spanEle.innerHTML = ele[i].tagName;
+
+            ele[i].appendChild(spanEle);
+            spanEle.addEventListener("click", function(e){
+
+                var parentElement = e.currentTarget.parentElement;
+
+                var parentId = parentElement.getAttribute("id");
+
+                var tagName = parentElement.tagName;
+
+                var className = parentElement.className;
+
+                var innerhtml = parentElement.innerHTML;
+
+                var alertMsg = parentId + " " + tagName + " " + className + " " + innerhtml;
+
+                alert(alertMsg);
+            });
+        }
+    }
+}
